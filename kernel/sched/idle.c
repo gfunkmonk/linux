b2a51435f5fd73c1ff8e0cc02494e2830c33191b
gfunkmonk <gfunkmonk@mailbox.org>
2026-04-30 01:39:29 -0400

    7

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -297,6 +297,9 @@ static void do_idle(void)
 	__current_set_polling();
 	tick_nohz_idle_enter();
 
+	/* POC Selector: mark CPU as idle */
+	set_cpu_idle_state(cpu, 1);
+
 	while (!need_resched()) {
 
 		/*
@@ -354,6 +357,9 @@ static void do_idle(void)
 		got_tick = tick_nohz_idle_got_tick();
 		arch_cpu_idle_exit();
 	}
+
+	/* POC Selector: mark CPU as busy */
+	set_cpu_idle_state(cpu, 0);
 
 	/*
 	 * Since we fell out of the loop above, we know TIF_NEED_RESCHED must
